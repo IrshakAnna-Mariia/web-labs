f5a21debc02220e246a4d7d5d4de9cf54d49a519
IrshakAnna-Mariia <irshakam@gmail.com>
2021-12-16 22:29:20 +0200

    refactor: add ECMAScript modules

--- lab2/src/index.js
+++ lab2/src/index.js
@@ -1,12 +1,7 @@
-const menuIcon = document.getElementById('menuIcon');
-const bagIcon = document.getElementById('bagIcon')
-const menu = document.getElementById('menu');
-const content = document.getElementById('content');
+import menuIcon from './helpers/getMenuIcon';
+import bagIcon from './helpers/getBagIcon';
+import handleMenu from './helpers/handleMenu';
 
-const handleMenu = () => {
-    menu.classList.toggle("hidden");
-    content.classList.toggle("hidden");
-}
 
 menuIcon.onclick = handleMenu;
 bagIcon.onclick = handleMenu;
